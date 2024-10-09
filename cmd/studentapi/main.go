d596974b381cc9d2f29a1c19a4ab2fbf363b55a8
shyam kuntal <72796245+shyamkuntal@users.noreply.github.com>
2024-10-09 08:17:18 +0530

    graceful shutdown

--- cmd/studentapi/main.go
+++ cmd/studentapi/main.go
@@ -1,9 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"github.com/shyamkuntal/studentapi/internal/config"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
 
 func main() {
 
-	fmt.Println("Welcome to Go Project Of Student API ")
+	// load config
+	cfg := config.MustLoad()
+
+	//setup router
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to student api"))
+	})
+
+	//setup server
+	server := http.Server{
+		Addr:    cfg.Addr,
+		Handler: router,
+	}
+
+	slog.Info("server started %s", slog.String("address",cfg.Addr))
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Fatal("Failed to start")
+		}
+	}()
+
+	<-done
+
+	slog.Info("shutting down the server")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err := server.Shutdown(ctx)
+	if err != nil {
+		slog.Error("Failed to shut down the server", slog.String("error", err.Error()))
+	}
+
+	slog.Info("server shut down successfully")
 
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -4,20 +4,19 @@ import (
 	"flag"
 	"log"
 	"os"
-
 	"github.com/ilyakaznacheev/cleanenv"
 )
 
 type HTTPServer struct {
-	Addr string
+	Addr string `yaml:"address" env-required:"true"`
 }
 
 // env-default:"production"
 
 type Config struct {
 	Env string `yaml:"env" env:"ENV" env-required:"true"`
-	StoragePath string `yaml:"storage_path env-required:"true"`
-	HTTPServer `yaml:"http_server`
+	StoragePath string `yaml:"storage_path" env-required:"true"`
+	HTTPServer `yaml:"http_server"`
 }
 
 func MustLoad() *Config{
